9b5b2d198f404e781d876ca03d19b094439be084
Šárka Slavětínská <sarka.slavetinska@performax.cz>
2023-08-19 13:31:49 +0200

    restructure

--- prog.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-erl -pa ./build/dev/erlang/*/ebin -eval 'markdown:main(), halt().' -noshell
--- run-tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-gleam build
-python spec_test/spec_tests.py --spec spec.txt --program ./prog.sh $@ | colordiff
